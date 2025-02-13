e1e133799fcdffeb39cb99ce72349c9f5a325bed
Y39WebDeveloper <y39freelancer@gmail.com>
2025-02-13 22:14:59 +0100

    .

---
+++ script.js
@@ -0,0 +1,12 @@
+// const btn = document.getElementById("btn")
+// let a = document.createElement("input")
+// btn.addEventListener("click", () => {
+//     let txt = document.querySelectorAll("input + p.txt-red")
+//     txt.forEach(err => {
+//         err.style.display = "none";
+//     })
+//     let errorTxt = document.querySelectorAll("input:invalid + p.txt-red")
+//     errorTxt.forEach(err => {
+//         err.style.display = "block";
+//     })
+// })
