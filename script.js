126b3dd77037e66dabbc0e762357dfce707180e0
Y39WebDeveloper <y39freelancer@gmail.com>
2025-02-14 18:02:06 +0100

    finale result

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-// const btn = document.getElementById("btn")
-// let a = document.createElement("input")
-// btn.addEventListener("click", () => {
-//     let txt = document.querySelectorAll("input + p.txt-red")
-//     txt.forEach(err => {
-//         err.style.display = "none";
-//     })
-//     let errorTxt = document.querySelectorAll("input:invalid + p.txt-red")
-//     errorTxt.forEach(err => {
-//         err.style.display = "block";
-//     })
-// })
+const btn = document.getElementById("btn")
+let a = document.createElement("input")
+btn.addEventListener("click", () => {
+    let txt = document.querySelectorAll("p.txt-red")
+    txt.forEach(err => {
+        err.style.display = "none";
+    })
+    let errorTxt = document.querySelectorAll("input:invalid + p.txt-red")
+    let radioErr = document.querySelectorAll("fieldset:has(input:invalid) p.txt-red")
+    let areaErr = document.querySelectorAll("textarea:invalid + p.txt-red")
+    let checkErr = document.querySelectorAll(".checkbox:has(input:invalid) + p.txt-red")
+    errorTxt.forEach(err => {
+        err.style.display = "block";
+    })
+    areaErr.forEach(err => {
+        err.style.display = "block";
+    })
+    radioErr.forEach(err => {
+        err.style.display = "block";
+    })
+    checkErr.forEach(err => {
+        err.style.display = "block";
+    })
+})
+
+let form = document.getElementById("form");
+let success = document.getElementById("success");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    success.classList.add("active");
+    setTimeout(() => {
+        success.classList.remove("active")
+    }, 2000);
+})
